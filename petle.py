7c4e6be6d4498399cac40274329deb83676c4c26
Łukasz Adamczak <aadamczak@edu.cdv.pl>
2019-05-30 21:59:48 +0200

    update

--- petle.py
+++ petle.py
@@ -2,9 +2,8 @@ fruits = ['apple','orange','pear','banana','apple']
 
 print("START")
 for i,fruit in enumerate(fruits):
-    print("Sprawdzam ->",i)
+    print("Sprawdzam {} {}".format("siema","elo"))
     if i==3:
-        print("TUTAJ")
         break
     print(i,":",fruit)
 
