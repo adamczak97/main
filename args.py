20933772b39a935f51a6cadbca01af6465310407
adamczak97 <aadamczak@edu.cdv.pl>
2019-05-31 21:15:04 +0200

    Zad 2

---
+++ args.py
@@ -0,0 +1,18 @@
+def parametr_args(argument, *args):
+    print("zawartość args: {}".format(args))
+    print("argument nazwany: {}".format(argument))
+    for arg in args:
+        print("argument z *args: {}".format(arg))
+
+parametr_args('python', 'spam', 'eggs', 'test',)
+
+print ("\n")
+
+def parametr_kwargs(argument, **kwargs):
+    print("argument: {}".format(argument))
+    print("zawartość kwargs: {}".format(kwargs))
+
+parametr_kwargs(dodatkowy=48, nastepny=111, argument=12)
+
+# argument: 12
+# zawartość kwargs: {'dodatkowy': 48, 'nastepny': 111}
