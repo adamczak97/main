b228727045ef3bd44faa65fe7e4770d4964aa45d
adamczak97 <aadamczak@edu.cdv.pl>
2019-05-31 23:25:03 +0200

    Zad 3 (pitagoras)

--- pitagoras.py
+++ pitagoras.py
@@ -19,5 +19,7 @@ else:
 
 if (a1*a1) + (b1*b1) == c1*c1:
     print("Ten trójkąt jest trójkątem pitagorejskim")
+    if a1 == 0.75*b1 and c1 == 1.25*b1:
+        print("Trójkat jest dodatkowo trójkątem egipskim")
 else:
     print("Ten trójkąt nie jest trójkątem pitagorejskim")
